2d0a86b029941b901c09ec4caed6d4be0c4b8591
alereyn <alereyn@hotmail.com>
2015-02-27 14:46:58 -0400

    commit 70

--- Sistema_Facturacion/My Project/Application.Designer.vb
+++ Sistema_Facturacion/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.Sistema_Facturacion.frmrptReImpresionFactura
+            Me.MainForm = Global.Sistema_Facturacion.frmLogin
         End Sub
     End Class
 End Namespace
